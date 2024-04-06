5582d882492b7209d4fa144b21ccd347865695d0
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-04-06 12:58:01 +0900

    Add project

---
+++ Main/العربية.py
@@ -0,0 +1,31 @@
+﻿# Imports
+import openai as افتحالذكاءالاصطناعي
+
+# Constants
+حقيقي = True
+خطأشنيع = False
+قائمةفارغة = [];
+
+# Input Output
+def مدخل():
+    return input()
+
+def مطبعة(قيمة):
+    print(قيمة)
+
+# 四則演算
+def يضيف(أ, ب):
+    return أ + ب
+
+def طرحاوخصم(أ, ب):
+    return أ - ب
+
+def تتضاعف(أ, ب):
+    return أ * ب
+
+def قسم(أ, ب):
+    return أ / ب
+
+# Functions
+def يفتح(ملف, وضع, التشفير):
+    return open(ملف, وضع, encoding = التشفير)
---
+++ Main/روبوت محادثة الذكاء الاصطناعي.py
@@ -0,0 +1,19 @@
+﻿from العربية import *
+
+with يفتح("./OpenAI_API_Key.txt", "r", "utf-8") as ملف:
+    افتحالذكاءالاصطناعي.api_key = ملف.readline()
+
+رسائل = قائمةفارغة
+
+while حقيقي:
+    محتوىالمستخدم = مدخل()
+
+    رسائل.append({ "role": "user", "content": f"{محتوىالمستخدم}" })
+
+    انتهاء = افتحالذكاءالاصطناعي.ChatCompletion.create(model = "gpt-3.5-turbo", messages = رسائل)
+
+    مساعدالمحتوى = انتهاء["choices"][0]["message"]["content"].strip()
+
+    رسائل.append({ "role": "assistant", "content": f"{مساعدالمحتوى}" })
+
+    مطبعة(f"الذكاء الاصطناعي : {مساعدالمحتوى}")
